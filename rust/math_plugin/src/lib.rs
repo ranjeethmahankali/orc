72a9220cfe6a958fb699cafdb02765a82d5fad4a
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2026-07-25 16:34:43 -0400

    Verify I can use the orc stuff from the plugin

--- rust/math_plugin/src/lib.rs
+++ rust/math_plugin/src/lib.rs
@@ -1,3 +1,31 @@
+use orc_sdk::{OrcHandle, OrcHost, OrcItemProxy, OrcMark, OrcPlugin, OrcTypeId, TOrcPluginAdaptor};
+
+struct Adaptor; // Unit struct to implement the adaptor.
+
+impl TOrcPluginAdaptor for Adaptor {
+    fn plugin_init(host: &OrcHost, out: &mut OrcPlugin) {
+        todo!()
+    }
+
+    fn deck_alloc(id: OrcTypeId) -> OrcHandle {
+        todo!()
+    }
+
+    fn deck_free(handle: &mut OrcHandle) {
+        todo!()
+    }
+
+    fn deck_from_proxy(
+        inputs: &[OrcHandle],
+        proxy_type: orc_sdk::ProxyType,
+        proxies: &[OrcItemProxy],
+        marks: &[OrcMark],
+        out: &mut OrcHandle,
+    ) {
+        todo!()
+    }
+}
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
--- rust/orc_sdk/src/lib.rs
+++ rust/orc_sdk/src/lib.rs
@@ -5,6 +5,7 @@ mod ffi;
 pub use ffi::{ProxyType, TOrcPluginAdaptor, dims_divide, dims_multiply, dims_pow};
 
 mod bindings;
+pub use bindings::{OrcHandle, OrcHost, OrcItemProxy, OrcMark, OrcPlugin, OrcTypeId};
 
 mod error;
 pub use error::Error;
