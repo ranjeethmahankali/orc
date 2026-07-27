6aab6c7c5b2f7c0d47cc3e5e4ad28bcdd15a2a15
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2026-07-26 21:22:16 -0400

    Support custom names to functions

--- rust/orc_sdk/derive_macros/src/lib.rs
+++ rust/orc_sdk/derive_macros/src/lib.rs
@@ -35,12 +35,43 @@ fn info_const_name(fn_name: &str) -> syn::Ident {
 ///     func: Some(plugin_fn_add),
 /// };
 /// ```
+struct OrcFnAttr {
+    name: Option<syn::LitStr>,
+}
+
+impl Parse for OrcFnAttr {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        if input.is_empty() {
+            return Ok(OrcFnAttr { name: None });
+        }
+        let mut name = None;
+        while !input.is_empty() {
+            let key: syn::Ident = input.parse()?;
+            if key == "name" {
+                let _: syn::Token![=] = input.parse()?;
+                name = Some(input.parse()?);
+            } else {
+                return Err(syn::Error::new(key.span(), format!("unknown attribute `{key}`")));
+            }
+            if !input.is_empty() {
+                let _: syn::Token![,] = input.parse()?;
+            }
+        }
+        Ok(OrcFnAttr { name })
+    }
+}
+
 #[proc_macro_attribute]
-pub fn orc_fn(_attr: TokenStream, item: TokenStream) -> TokenStream {
+pub fn orc_fn(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let attr = parse_macro_input!(attr as OrcFnAttr);
     let func = parse_macro_input!(item as ItemFn);
     let fn_name = &func.sig.ident;
     let fn_name_str = fn_name.to_string();
     let const_name = info_const_name(&fn_name_str);
+    let display_name = attr
+        .name
+        .map(|lit| lit.value())
+        .unwrap_or_else(|| fn_name_str.clone());
     // Extract doc comments from attributes.
     let doc_lines: Vec<String> = func
         .attrs
@@ -65,8 +96,8 @@ pub fn orc_fn(_attr: TokenStream, item: TokenStream) -> TokenStream {
         .join(" ")
         .trim()
         .to_string();
-    // Build C string literals. We use byte-string concat at the call site.
-    let name_cstr = format!("{}\0", fn_name_str);
+    // Build C string literals.
+    let name_cstr = format!("{}\0", display_name);
     let desc_cstr = format!("{}\0", desc);
     let name_bytes = proc_macro2::Literal::byte_string(name_cstr.as_bytes());
     let desc_bytes = proc_macro2::Literal::byte_string(desc_cstr.as_bytes());
