0aee07c255f2912ef45d333ec93eb78a80773028
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2026-08-22 13:49:06 -0400

    Pull rmp dep and add stub

--- rust/orc_sdk/src/dag/io.rs
+++ rust/orc_sdk/src/dag/io.rs
@@ -1,4 +1,5 @@
 use super::{DagError, Graph, Workflow};
+use rmp::encode::RmpWrite;
 use std::path::Path;
 
 const WORKFLOW_VERSION_CURRENT: u64 = 1;
@@ -10,5 +11,17 @@ impl Workflow {
 }
 
 impl Graph {
-    pub fn write_to_msgpack() {}
+    pub fn write_to_msgpack(&self, w: &mut impl RmpWrite) -> Result<(), DagError> {
+        if self.inputs.iter().any(|i| i.deleted)
+            || self.outputs.iter().any(|i| i.deleted)
+            || self.links.iter().any(|i| i.deleted)
+            || self.nodes.iter().any(|i| i.deleted)
+        {
+            // We don't want to ever serialize a graph with stale deleted elements. Before saving to
+            // a file, the caller must first clean up all the stale elements from the graph by
+            // calling garbage collection.
+            return Err(DagError::GarbageCollectionRequired);
+        }
+        todo!();
+    }
 }
